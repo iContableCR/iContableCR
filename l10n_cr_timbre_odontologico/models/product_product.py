8a108d84ae047fb666f0f5a64b572107ebea0a84
nicolas <nicolas>
2023-03-26 01:09:30 -0500

    [FIX] corregir reporte factura

--- l10n_cr_timbre_odontologico/models/product_product.py
+++ l10n_cr_timbre_odontologico/models/product_product.py
@@ -10,9 +10,10 @@ class ProductProduct(models.Model):
 
     exent_product = fields.Boolean("Afecta Timbre Odontologico",related="product_tmpl_id.exent_product")
     economic_activity_id = fields.Many2one("economic.activity","Actividad económica por defecto",related="product_tmpl_id.economic_activity_id")
-
+    service_medic = fields.Boolean("Es Servicio Medico", related='product_tmpl_id.service_medic')
 class ProductElectronic(models.Model):
     _inherit = "product.template"
 
     exent_product = fields.Boolean("Afecta Timbre Odontologico")
     economic_activity_id = fields.Many2one("economic.activity","Actividad económica por defecto",domain="[('active','=',True)]")
+    service_medic = fields.Boolean("Es Servicio Medico")
