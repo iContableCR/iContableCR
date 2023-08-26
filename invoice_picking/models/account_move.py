2fa15981d54a53449553c5e8593b8f4818429697
nicolas <nicolas>
2023-08-25 21:06:31 -0500

    [ADD] agregar invoice_picking

---
+++ invoice_picking/models/account_move.py
@@ -0,0 +1,80 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    picking_count = fields.Integer("Entregas",compute="compute_stock_picking",copy=False)
+    stock_picking_ids = fields.Many2many(comodel_name="stock.picking",string="Transferencias",relation='account_moves_stock_pickings_rel',copy=False)
+
+    @api.depends("stock_picking_ids")
+    def compute_stock_picking(self):
+        for order in self:
+            order.picking_count = len(order.stock_picking_ids)
+
+    def prepare_values_stock_picking(self,picking_type):
+        values = {
+            'picking_type_id': picking_type.id,
+            'partner_id': self.partner_id.id,
+            'origin': self.name,
+            'move_type': 'direct'
+        }
+        if picking_type.code == 'outgoing':
+            values.update({
+                'location_dest_id': self.partner_id.property_stock_customer.id,
+                'location_id': picking_type.default_location_src_id.id,    
+            })
+        if picking_type.code == 'incoming':
+            values.update({
+                'location_dest_id': picking_type.default_location_dest_id.id,
+                'location_id': self.partner_id.property_stock_supplier.id,
+            })
+        return values
+    
+    def create_stock_picking(self):
+        for order in self:
+            code = False
+            if self.move_type == 'out_invoice':
+                code = 'outgoing'
+            if self.move_type == 'in_invoice':
+                code = 'incoming'
+            if self.move_type == 'out_refund':
+                code = 'incoming'
+            if self.move_type == 'in_refund':
+                code = 'outgoing'
+            picking_type = self.env['stock.picking.type'].search([('code','=',code)],limit=1)
+            if picking_type:
+                pickings = self.env['stock.picking'].create(self.prepare_values_stock_picking(picking_type))
+                order.stock_picking_ids = [(6,0,pickings.ids)]
+                moves = order.invoice_line_ids.filtered(
+                    lambda r: r.product_id.type in ['product', 'consu']).create_stock_move(pickings)
+                move_ids = moves._action_confirm()
+                move_ids._action_assign()
+    
+    def action_post(self):
+        res = super(AccountMove,self).action_post()
+        if self.move_type in ('out_invoice','in_invoice','out_refund','in_refund'):
+            self.create_stock_picking()
+        return res
+    
+    def action_view_picking(self):
+        action = self.env.ref('stock.action_picking_tree_ready').read()[0]
+        action.pop('id', None)
+        action.update({
+            'context': {},
+            'domain': [('id', 'in', self.stock_picking_ids.ids)]
+        })
+        if self.stock_picking_ids:
+            res = self.env.ref('stock.view_picking_form', False)
+            action.update({
+                'views': [(res and res.id or False, 'form')],
+                'res_id': self.stock_picking_ids.id or False
+            })
+        return action
+
+    def _reverse_moves(self, default_values_list=None, cancel=False):
+        reverse_moves = super()._reverse_moves(default_values_list, cancel)
+        for moves in reverse_moves:
+            if moves.move_type in ('out_invoice','in_invoice','out_refund','in_refund'):
+                moves.create_stock_picking()
+        return reverse_moves
---
+++ invoice_picking/models/account_move_line.py
@@ -0,0 +1,40 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    def prepare_values_stock_move(self,line,picking):
+        price_unit = line.price_unit
+        values = {
+                    'name': line.name or '',
+                    'product_id': line.product_id.id,
+                    'product_uom': line.product_uom_id.id,
+                    'picking_id': picking.id,
+                    'state': 'draft',
+                    'company_id': line.move_id.company_id.id,
+                    'price_unit': price_unit,
+                    'picking_type_id': picking.picking_type_id.id,
+                    'route_ids': 1 and [
+                        (6, 0, [x.id for x in self.env['stock.location.route'].search([('id', 'in', (2, 3))])])] or [],
+                    'warehouse_id': picking.picking_type_id.warehouse_id.id,
+                    'product_uom_qty': line.quantity,
+                }
+        if picking.picking_type_id.code == 'outgoing':
+            values.update({
+                'location_id': picking.picking_type_id.default_location_src_id.id,
+                'location_dest_id': line.move_id.partner_id.property_stock_customer.id,
+            })
+        if picking.picking_type_id.code == 'incoming':
+            values.update({
+                'location_id': line.move_id.partner_id.property_stock_supplier.id,
+                'location_dest_id': picking.picking_type_id.default_location_dest_id.id,
+            })
+        return values
+
+    def create_stock_move(self, picking):
+        values = []
+        for move_line in self:
+            template = self.prepare_values_stock_move(move_line,picking)
+            values.append(template)
+        return self.env['stock.move'].create(values)
